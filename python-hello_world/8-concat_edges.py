cc5c9cb7ffb66026d5d0d91e5ac758fcf5781dcb
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-04-15 13:07:45 +0400

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "Python"
-str3 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str3[39:67] + str3[106:112] + str2)
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+
+print(str[39:67] + str[106:112] + str[:6])
+
