ee438f91910b02aecd22b8974c0ac7f650f69801
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-04-15 13:52:12 +0400

    Task 6: area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
