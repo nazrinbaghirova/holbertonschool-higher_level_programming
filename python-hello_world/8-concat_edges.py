90f6b0a3b79179caf2eed0ed20f90ffdde2c3ff6
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-04-15 13:03:30 +0400

    final fix task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "Python"
-str3 = "is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str3 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 print(str3[39:67] + str3[106:112] + str2)
