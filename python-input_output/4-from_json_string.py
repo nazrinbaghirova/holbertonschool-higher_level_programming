2a2afd3c91cd265e6ca9f8a9b7ad08b1b9d5ba1d
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-04-21 09:46:04 +0400

    Fix task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This module provides a function to convert a JSON string to a Python object."""
+"""JSON string to Python object."""
 
 import json
 
 
 def from_json_string(my_str):
-    """Return the Python object represented by a JSON string."""
+    """Return Python object from JSON string."""
     return json.loads(my_str)
