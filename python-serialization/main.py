b5797f35f1a1c41057111be01f939946750caf2f
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-04-21 10:14:50 +0400

    Add task 1

--- python-serialization/main.py
+++ python-serialization/main.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-from task_00_basic_serialization import load_and_deserialize, serialize_and_save_to_file
+from task_01_pickle import CustomObject
 
-data_to_serialize = {
-    "name": "John Doe",
-    "age": 30,
-    "city": "New York"
-}
+obj = CustomObject(name="John", age=25, is_student=True)
 
-serialize_and_save_to_file(data_to_serialize, 'data.json')
-print("Data serialized and saved to 'data.json'.")
+print("Original Object:")
+obj.display()
 
-deserialized_data = load_and_deserialize('data.json')
+obj.serialize("object.pkl")
 
-print("Deserialized Data:")
-print(deserialized_data)
+new_obj = CustomObject.deserialize("object.pkl")
+
+print("\nDeserialized Object:")
+if new_obj:
+    new_obj.display()
 
