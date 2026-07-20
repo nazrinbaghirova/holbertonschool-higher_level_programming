a96e7b2b5f17043427a4e6d5b159a2f1384e15cf
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-07-20 10:36:57 +0400

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str2 = "Holberton School"
 print(str1[39:66] + str1[106:112] + str1[:6])
