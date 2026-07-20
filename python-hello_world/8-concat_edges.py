840b5910a06d9fa006ed082da7ed7c3b76572c3e
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-07-20 10:34:37 +0400

    Complete 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+str1 = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6]
-print(str)
+str2 = "Holberton School"
+print(str1[39:66] + str1[106:112] + str1[:6])
